c0ed92ab38780eeb6fb5c268e8c3d6ea5058efd5
testuser <testuser@debianVM.myguest.virtualbox.org>
2024-10-15 22:39:42 +0300

    Changed name of proj in CMake

---
+++ build/CMakeFiles/BankApp.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "BankApp"
+  "BankApp.pdb"
+  "CMakeFiles/BankApp.dir/AccountController.cpp.o"
+  "CMakeFiles/BankApp.dir/AccountController.cpp.o.d"
+  "CMakeFiles/BankApp.dir/BankAccount.cpp.o"
+  "CMakeFiles/BankApp.dir/BankAccount.cpp.o.d"
+  "CMakeFiles/BankApp.dir/main.cpp.o"
+  "CMakeFiles/BankApp.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BankApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
