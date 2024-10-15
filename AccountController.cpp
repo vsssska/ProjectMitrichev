5bd1d7b5ebed10bfb3e3b24aa9144a017a6a50f1
testuser <testuser@debianVM.myguest.virtualbox.org>
2024-10-15 22:09:05 +0300

    Added withdraw, getBalance in AccountController.cpp

--- AccountController.cpp
+++ AccountController.cpp
@@ -13,4 +13,38 @@ void AccountController::deposit(const drogon::HttpRequestPtr& req, std::function
 	}).detach();
 }
 
+void AccountController::withdraw(const drogon::HttpRequestPtr& req, std::function<void (const drogon::HttpResponsePtr &)> &&callback) { //double amount) {
+	auto amount = std::stod(req->getParameter("amount"));//.asDouble();
+
+	std::thread([this, amount, callback] () {
+		//{
+			//std::unique_lock<std::mutex> lock{account.getMutex()};
+			if (account.getBalance() < amount ) {
+				auto resp = drogon::HttpResponse::newHttpResponse();
+                		resp->setBody("Insufficient funds to withdraw  " + std::to_string(amount));
+                		callback(resp);
+				//return;
+				
+				account.waitForFunds(amount);
+			}
+			account.withdraw(amount);
+		//}
+
+		auto resp = drogon::HttpResponse::newHttpResponse();
+		/*if(account.getBalance() < amount) 
+                	resp->setBody("Insufficient funds to withdraw  " + std::to_string(amount));
+		else 
+                	resp->setBody("Withdraw successful, you've withdrawn  " + std::to_string(amount)); */
+		resp->setBody("Withdraw successful, you've withdrawn " + std::to_string(amount));
+		callback(resp);
+		//account.withdraw(amount);
+	}).detach();
+}
+
+
+void AccountController::getBalance(const drogon::HttpRequestPtr& req, std::function<void (const drogon::HttpResponsePtr &)> &&callback) {
+	auto resp = drogon::HttpResponse::newHttpResponse();
+	resp->setBody("Current Balance: " + std::to_string(account.getBalance()));
+	callback(resp);
+}
 
