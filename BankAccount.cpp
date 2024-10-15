35889a48811912c65f74a98316f129e1cfb6cb62
testuser <testuser@debianVM.myguest.virtualbox.org>
2024-10-15 21:31:04 +0300

    Added function Wihdraw, getter for balance, mtx and waitForFund inBankAccount class

--- BankAccount.cpp
+++ BankAccount.cpp
@@ -11,5 +11,28 @@ void BankAccount::deposit(double amount) {
 	cv.notify_all();
 }
 
+void BankAccount::withdraw(double amount) {
+		std::unique_lock<std::mutex> lock{mtx};
+		//cv.wait(lock, [this, amount]() {return balance >= amount; });
+		if (balance > 0) balance -= amount;
+		else balance = 0;
+		//cv.notify_all();
+}
+
+double BankAccount::getBalance() const  {
+	std::lock_guard<std::mutex> lock{mtx};
+
+	return balance;
+}
+
+std::mutex& BankAccount::getMutex() const {
+	return mtx;
+
+}
+
+void BankAccount::waitForFunds(double amount) {
+	std::unique_lock<std::mutex> lock{mtx};
+	cv.wait(lock, [this, amount]() { return balance >= amount; });
+}
 
 
